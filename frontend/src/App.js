655a295d8c48d7a50565a503b72213c608a8b8cc
Daniel <114535778+danielbmata@users.noreply.github.com>
2024-11-11 10:38:43 -0300

    page navigation with react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
+import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Create from './components/Create';
+import About from './components/About'; 
+
 
 function App() {
   return (
     <div className="App">
-      <div>Our aplication =D</div>      
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/create" element={<Create />} /> 
+        <Route path="/about" element={<About />} /> 
+      </Routes>      
     </div>
   );
 }
 
 export default App;
+// page navigation with react router
