f978b33e14c4019a82b0de1ffb7fb25212edb874
Daniel <114535778+danielbmata@users.noreply.github.com>
2024-11-12 10:52:14 -0300

    feat: add Material UI navbar with responsive design

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# django-react
+# Django & React Full Stack Application
+This project is a full stack application using Django for the backend and React for the frontend. The goal is to create a user-friendly and responsive interface with page navigation, Material UI integration, and basic CRUD operations.
+
+Technologies Used
+Backend: Django (Python)
+Frontend: React.js
+Libraries: React Router, Material UI
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,28 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Create from './components/Create';
-import About from './components/About'; 
-
+import About from './components/About';
+import NavBar from './components/NavBar';
 
 function App() {
+  const myWidth = 210 // largura do sidebar
   return (
     <div className="App">
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="/create" element={<Create />} /> 
-        <Route path="/about" element={<About />} /> 
-      </Routes>      
+      <NavBar
+        drawerWidth={myWidth}
+        content={
+
+          <Routes>
+            <Route path="" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+
+        }
+
+      />
     </div>
   );
 }
 
 export default App;
-// page navigation with react router
